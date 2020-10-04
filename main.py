68d61711e85e2bb00eb99a6cd47878c8f67c36c8
jd20121 <jdkwyo@gmail.com>
2020-10-04 03:52:13 +0000

    options commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import WazeRouteCalculator
-from csv import writer
+from csv import writer 
 
 # github: https://github.com/jd2012/Waze-Traffic-Calculator
 
@@ -8,19 +8,40 @@ to_address = ["Peoria, IL", "Peoria, IL", "Peoria, IL", "Bloomington, IL", "Bloo
 region = 'US'
 header_row = ["From", "To", "Travel Time"]
 
-def pick_format(){
-  print("Choose the format to print to the console:\n\t(1) Times inline context\n\t(2) Comma seperated times and context \n\t(3) Just times")
-  print_format = input("Choose format [1, 2, 3]:")
-}
-if 
+
+def pick_format():
+  global fp
+  fp = input("Choose format [1,2,3]: ")
+  check_format()
 
 
-with open('traffictimes.csv', 'w', newline='') as file:
-  w = writer(file)
-  w.writerow(header_row)
-  for i in range(8):
-    route = WazeRouteCalculator.WazeRouteCalculator(from_address[i], to_address[i], region)
-    route_time, route_distance = route.calc_route_info()
-    print("From %s to %s:\t %.2f minutes" % (from_address[i], to_address[i], route_time))
-    formatted_route_time = "{:.2f}"
-    w.writerow((from_address[i], to_address[i], formatted_route_time.format(route_time)))
+def check_format():
+  if fp in {"1", "2", "3"}:
+    write_times()
+  else:
+    print('\a')
+    print("Error: Invalid Format")
+    pick_format()
+
+def write_times():
+  with open('traffictimes.csv', 'w', newline='') as file:
+    w = writer(file)
+    w.writerow(header_row)
+    for i in range(8):
+      route = WazeRouteCalculator.WazeRouteCalculator(from_address[i], to_address[i], region)
+      route_time, route_distance = route.calc_route_info()
+      if fp == 1:
+        print("From %s to %s:\t %.2f minutes" % (from_address[i], to_address[i], route_time))
+      elif fp == 2:
+        print("%s,%s,%.2f" % (from_address[i], to_address[i], route_time))
+      elif fp == 3:
+        print("%.2f" % (route_time))
+      else:
+        print("Error: Please try again")
+      
+
+      formatted_route_time = "{:.2f}"
+      w.writerow((from_address[i], to_address[i], formatted_route_time.format(route_time)))
+
+print("Choose the format to print to the console:\n\t(1) Times inline with context\n\t(2) Comma seperated times and context \n\t(3) Just times")
+pick_format()
